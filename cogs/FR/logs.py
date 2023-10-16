836613be244fae319f4ffeaeb04368aaba686463
Awakno <95168660+PolarisWolf@users.noreply.github.com>
2023-10-16 11:30:48 +0200

    Ajout d'event logs

--- cogs/FR/logs.py
+++ cogs/FR/logs.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 import json
+
 class LogEvents(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -25,7 +26,7 @@ class LogEvents(commands.Cog):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Message reçu", value=f">>> Auteur : {message.author.mention}\nContenu : {message.content}\nCanal : {message.channel.mention}")
+            embed.add_field(name="Message reçu", value=f">>> Auteur: {message.author.mention}\nContenu: {message.content}\nCanal: {message.channel.mention}")
             embed.set_author(name=f"{message.author}", icon_url=message.author.display_avatar.url)
             embed.set_footer(text=f"ID de l'auteur : {message.author.id}")
             await log_channel.send(embed=embed)
@@ -35,7 +36,7 @@ class LogEvents(commands.Cog):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Membre a rejoint", value=f">>> {member.mention}\nServeur : {member.guild.name}")
+            embed.add_field(name="Membre a rejoint", value=f">>> {member.mention}\nServeur: {member.guild.name}")
             embed.set_author(name=f"{member}", icon_url=member.display_avatar.url)
             embed.set_footer(text=f"ID du membre : {member.id}")
             await log_channel.send(embed=embed)
@@ -45,7 +46,7 @@ class LogEvents(commands.Cog):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Membre a quitté", value=f">>> {member.mention}\nServeur : {member.guild.name}")
+            embed.add_field(name="Membre a quitté", value=f">>> {member.mention}\nServeur: {member.guild.name}")
             embed.set_author(name=f"{member}", icon_url=member.display_avatar.url)
             embed.set_footer(text=f"ID du membre : {member.id}")
             await log_channel.send(embed=embed)
@@ -56,7 +57,7 @@ class LogEvents(commands.Cog):
         if log_channel:
             if before.nick != after.nick:
                 embed = discord.Embed(timestamp=datetime.now())
-                embed.add_field(name="Changement de surnom", value=f">>> {after.mention}\nNouveau surnom : {after.nick}")
+                embed.add_field(name="Changement de surnom", value=f">>> {after.mention}\nNouveau surnom: {after.nick}")
                 embed.set_author(name=f"{after}", icon_url=after.display_avatar.url)
                 embed.set_footer(text=f"ID du membre : {after.id}")
                 await log_channel.send(embed=embed)
@@ -66,7 +67,6 @@ class LogEvents(commands.Cog):
                 added_roles_text = " ".join([f"{role.mention}" for role in added_roles])
                 removed_roles_text = " ".join([f"{role.mention}" for role in removed_roles])
                 embed = discord.Embed(timestamp=datetime.now())
-                
                 if added_roles:
                     embed.add_field(name="Rôles ajoutés", value=f">>> {added_roles_text}")
                 if removed_roles:
@@ -78,25 +78,27 @@ class LogEvents(commands.Cog):
                 embed.set_author(name=f"{after}", icon_url=after.display_avatar.url)
                 embed.set_footer(text=f"ID du membre : {after.id}")
                 await log_channel.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel and before.content != after.content:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Message modifié", value=f">>> Auteur: {after.author.mention}\nAncien message : {before.content}\nNouveau message : {after.content}")
+            embed.add_field(name="Message modifié", value=f">>> Auteur: {after.author.mention}\nAncien message: {before.content}\nNouveau message: {after.content}")
             embed.set_author(name=f"{after.author}", icon_url=after.author.display_avatar.url)
             embed.set_footer(text=f"ID du membre : {after.author.id}")
             await log_channel.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_guild_role_create(self, role):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Rôle créé", value=f"Nom : {role.name}\nID : {role.id}\nCouleur : {role.color}")
+            embed.add_field(name="Rôle créé", value=f">>> Nom: {role.name}\nID: {role.id}\nCouleur: {role.color}")
             # Obtenir l'auteur de la création du rôle depuis le journal d'audit
             async for entry in role.guild.audit_logs(limit=1, action=discord.AuditLogAction.role_create):
                 author = entry.user
-            embed.add_field(name="Créé par", value=f"{author.mention}")
+            embed.add_field(name="Créé par", value=f"> {author.mention}")
             try:
                 embed.set_author(name="Rôle créé", icon_url=role.guild.icon.url)
             except:
@@ -108,16 +110,15 @@ class LogEvents(commands.Cog):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Rôle supprimé", value=f"Nom : {role.name}\nID : {role.id}")
+            embed.add_field(name="Rôle supprimé", value=f">>> Nom: {role.name}\nID: {role.id}")
             # Obtenir l'auteur de la suppression du rôle depuis le journal d'audit
             async for entry in role.guild.audit_logs(limit=1, action=discord.AuditLogAction.role_delete):
                 author = entry.user
-            embed.add_field(name="Supprimé par", value=f"{author.mention}")
+            embed.add_field(name="Supprimé par", value=f"> {author.mention}")
             try:
                 embed.set_author(name="Rôle supprimé", icon_url=role.guild.icon.url)
             except:
                 embed.set_author(name="Rôle supprimé")
-            
             await log_channel.send(embed=embed)
 
     @commands.Cog.listener()
@@ -126,7 +127,7 @@ class LogEvents(commands.Cog):
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
             if isinstance(channel, discord.TextChannel):
-                channel_type = "Salon textuel"
+                channel_type = "Salon texte"
             elif isinstance(channel, discord.VoiceChannel):
                 channel_type = "Salon vocal"
             elif isinstance(channel, discord.CategoryChannel):
@@ -135,13 +136,39 @@ class LogEvents(commands.Cog):
                 channel_type = "Thread"
             else:
                 channel_type = "Inconnu"
-            embed.add_field(name=f"Salon créé ({channel_type})", value=f"Nom : {channel.name}\nID : {channel.id}")
+            embed.add_field(name=f"Création de salon ({channel_type})", value=f"Nom: {channel.name}\nID: {channel.id}")
             async for entry in channel.guild.audit_logs(limit=1, action=discord.AuditLogAction.channel_create):
                 author = entry.user
-            embed.add_field(name="Créé par", value=f"{author.mention}")
-            embed.set_author(name=f"Salon créé ({channel_type})", icon_url=channel.guild.icon.url)
+            embed.add_field(name="Créé par", value=f"> {author.mention}")
+            embed.set_author(name=f"Création de salon ({channel_type})", icon_url=channel.guild.icon.url)
             embed.set_footer(text=f"ID du serveur : {channel.guild.id}")
             await log_channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_guild_channel_update(self, before, after):
+        log_channel = self.bot.get_channel(self.log_channel_id)
+        if log_channel:
+            embed = discord.Embed(timestamp=datetime.now())
+            if isinstance(after, discord.TextChannel):
+                channel_type = "Salon texte"
+            elif isinstance(after, discord.VoiceChannel):
+                channel_type = "Salon vocal"
+            elif isinstance(after, discord.CategoryChannel):
+                channel_type = "Catégorie"
+            elif isinstance(after, discord.Thread):
+                channel_type = "Thread"
+            else:
+                channel_type = "Inconnu"
+            
+            embed.add_field(name=f"Salon mis à jour ({channel_type})", value=f"Nom: {before.name} (ID: {before.id})")
+            embed.add_field(name="Changements", value=f"Modifié en : {after.name} (ID: {after.id})")
+            async for entry in after.guild.audit_logs(limit=1, action=discord.AuditLogAction.channel_update):
+                author = entry.user
+            embed.add_field(name="Modifié par", value=f"> {author.mention}")
+            embed.set_author(name=f"Salon mis à jour ({channel_type})", icon_url=after.guild.icon.url)
+            embed.set_footer(text=f"ID du serveur : {after.guild.id}")
+            await log_channel.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_member_kick(self, guild, member):
         log_channel = self.bot.get_channel(self.log_channel_id)
@@ -149,7 +176,7 @@ class LogEvents(commands.Cog):
             embed = discord.Embed(timestamp=datetime.now())
             action_type = "Expulsion"
             reason = "Aucune raison fournie"
-            embed.add_field(name=f"{action_type} d'un membre", value=f"Utilisateur : {member.mention}\nServeur : {guild.name}")
+            embed.add_field(name=f"{action_type} d'un membre", value=f"> Membre : {member.mention}\nServeur: {guild.name}")
             embed.add_field(name="Raison", value=reason)
             async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.kick):
                 if entry.target == member:
@@ -160,20 +187,52 @@ class LogEvents(commands.Cog):
             await log_channel.send(embed=embed)
 
     @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        log_channel = self.bot.get_channel(self.log_channel_id)
+        if log_channel:
+            embed = discord.Embed(timestamp=datetime.now())
+            embed.add_field(name="Message supprimé", value=f"> Message : {message.content}")
+            embed.set_author(name=f"{message.author}", icon_url=message.author.display_avatar.url)
+            embed.set_footer(text=f"ID du membre : {message.author.id}")
+            await log_channel.send(embed=embed)
+
+    @commands.Cog.listener()
     async def on_member_ban(self, guild, user):
         log_channel = self.bot.get_channel(self.log_channel_id)
         if log_channel:
             embed = discord.Embed(timestamp=datetime.now())
-            embed.add_field(name="Membre banni", value=f"Utilisateur : {user.mention}\nServeur : {guild.name}")
+            embed.add_field(name="Membre banni", value=f"> Membre : {user.mention}\nServeur : {guild.name}")
             embed.add_field(name="Raison", value=user.ban_reason if isinstance(user, discord.Member) else "Aucune raison fournie")
             async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.ban):
                 if entry.target == user:
                     author = entry.user
-            embed.add_field(name="Par Bannissement", value=f"{author.mention}")
+            embed.add_field(name="En bannissant", value=f"> {author.mention}")
             embed.set_author(name=f"{user}", icon_url=user.display_avatar.url)
             embed.set_footer(text=f"ID du membre : {user.id}")
             await log_channel.send(embed=embed)
 
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel):
+        log_channel = self.bot.get_channel(self.log_channel_id)
+        if log_channel:
+            embed = discord.Embed(timestamp=datetime.now())
+            if isinstance(channel, discord.TextChannel):
+                channel_type = "Salon texte"
+            elif isinstance(channel, discord.VoiceChannel):
+                channel_type = "Salon vocal"
+            elif isinstance(channel, discord.CategoryChannel):
+                channel_type = "Catégorie"
+            elif isinstance(channel, discord.Thread):
+                channel_type = "Thread"
+            else:
+                channel_type = "Inconnu"
+            embed.add_field(name=f"Salon supprimé ({channel_type})", value=f"Nom: {channel.name}\nID: {channel.id}")
+            async for entry in channel.guild.audit_logs(limit=1, action=discord.AuditLogAction.channel_delete):
+                author = entry.user
+            embed.add_field(name="Supprimé par", value=f"> {author.mention}")
+            embed.set_author(name=f"Salon supprimé ({channel_type})", icon_url=channel.guild.icon.url)
+            embed.set_footer(text=f"ID du serveur : {channel.guild.id}")
+            await log_channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(LogEvents(bot))
--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+try:
+    import discord
+except:
+    print("Discord module not found. Install with `pip install discord`")
+    exit(1)
 
+
+
+print("All modules found! start the file `main.py`")
