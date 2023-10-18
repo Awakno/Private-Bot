c38b322329b19b1b0a25c2ec5f6b10314f41f145
PolarisWolf <teampolaris115@gmail.com>
2023-10-18 17:09:22 +0200

    Added admin english

--- cogs/EN/admin.py
+++ cogs/EN/admin.py
@@ -6,7 +6,22 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
-    #soon
+    @commands.command(name="setprefix")
+    async def set_prefix(self, ctx, prefix):
+        with open("config.json", "r") as f:
+            config = json.load(f)
+        
+        if ctx.author.id in config['owner']:
+            config['prefix'] = prefix  # Modifier la valeur de la clé 'prefix'
+            self.bot.command_prefix = prefix
+
+            # Enregistrer les changements dans le fichier JSON
+            with open("config.json", "w") as f:
+                json.dump(config, f, indent=4)
+
+            await ctx.send(f"Préfix changed ! new prefix is : `{prefix}`")
+        else:
+            await ctx.send("You are not owner of bot", delete_after=3)
     
 def setup(bot):
     bot.add_cog(Admin(bot))
