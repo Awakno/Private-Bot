05ebff2d5c3066212b24d807be5bbabee845f183
PolarisWolf <teampolaris115@gmail.com>
2023-10-18 19:10:46 +0200

    Add command avatar

---
+++ cogs/EN/avatar.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import discord
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="avatar")
+    async def avatar(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+        await ctx.send(embed=discord.Embed(title=f"🖼️ {member.name}'s Avatar").set_image(url=member.display_avatar.url))
+
+    @commands.command(name="servericon")
+    async def servericon(self, ctx):
+        await ctx.send(embed=discord.Embed(title="🖼️ Server Icon").set_image(url=ctx.guild.icon.url))
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
