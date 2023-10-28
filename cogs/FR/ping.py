44d16477ff057371211e879b0c1286aaf0b8500c
PolarisWolf <teampolaris115@gmail.com>
2023-10-28 16:45:13 +0200

    command ping

---
+++ cogs/FR/ping.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+class Ping(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+
+
+    @commands.command(name = "ping")
+    async def ping(self,ctx):
+        await ctx.send(f":ping_pong: *Pong!** {round(self.bot.latency * 1000)} ms")
+    
+
+
+def setup(bot:commands.Bot):
+    bot.add_cog(Ping(bot))
