7bdb38c7cdc273ba91e89ec49f54af1ea5387f83
Zerbaib <hugo.hennetin@code.catdev.fr>
2023-10-31 21:31:58 +0000

    better setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
+import os
+
 try:
-    import discord
-except:
-    print("Discord module not found. Install with `pip install discord`")
-    exit(1)
+    print("L'installation est en cours")
+    os.system("pip install -r requirements.txt")
+    try:
+        import discord
+        print("Succes !")
+    except:
+        print("Une erreur a eu lieux durant l'importation !")
+except Exception as e:
+    print("Une erreur a eu lieux durant l'installation !")
+    print(e)
 
+print("Voulez vous lancer le bot ? [o/n]")
+reponse = input('>>> ')
+rep = reponse.lower()
 
-
-print("All modules found! start the file `main.py`")
+if rep == "o":
+    os.system("python main.py")
+else:
+    print("bye")
