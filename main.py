9686760fa1be5a489e4e397d4ba03c4698002952
Zerbaib <hugo.hennetin@proton.me>
2023-11-05 11:59:55 +0100

    Fix language loading issue in main.py

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ bot = commands.Bot(
     command_prefix=config['prefix'],
     intents=discord.Intents.all()
     )
-lang = upper(config['lang'])
+lang = config['lang']
+lang = lang.upper()
 list_lang = []
 
 for folder in os.listdir("./cogs/"):
     list_lang.append(folder)
 
-if lang is in list_lang:
+if lang in list_lang:
     for file in os.listdir(f"./cogs/{lang}"):
         if file.endswith(".py"):
             bot.load_extension(f"cogs.{lang}.{file[:-3]}")
